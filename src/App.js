2d72244604c5bef5d9345951062671278c5b869c
innovativeGem <innovativeGem@gmail.com>
2021-08-17 19:13:07 +1000

    header, footer and logo added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import './App.scss';
+import {ReactComponent as AdoreLogo} from './adore-logo.svg';
+
+const DEVELOPER = 'Naman Nanavati';
+const TODAY = new Date();
+const YEAR = TODAY.getFullYear();
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Adore Beauty</h1>
+    <div className="root">
+      <header className="app-header">
+        <AdoreLogo className="logo" />
       </header>
+      <main className="app-content">Product list</main>
+      <footer className="app-footer">{DEVELOPER} - {YEAR}</footer>
     </div>
   );
 }
