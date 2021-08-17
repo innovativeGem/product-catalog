1dc54addf14f509224fe78a7ff60731e8ea058dc
innovativeGem <innovativeGem@gmail.com>
2021-08-17 22:39:42 +1000

    product list added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import './App.scss';
-import {ReactComponent as AdoreLogo} from './adore-logo.svg';
-import ProductList from './components/product/ProductList';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { useState } from 'react';
+import "./App.scss";
+import { ReactComponent as AdoreLogo } from "./adore-logo.svg";
+import ProductList from "./components/product/ProductList";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { useState, useEffect } from "react";
 
-const DEVELOPER = 'Naman Nanavati';
+const DEVELOPER = "Naman Nanavati";
 const TODAY = new Date();
 const YEAR = TODAY.getFullYear();
 
 function App() {
   const [isLoading, setLoading] = useState(true);
+  const [products, setProducts] = useState();
+  const axios = require("axios").default;
 
-  // Temporary
-  setTimeout(() => {
-    setLoading(false);
-  }, 500);
+  useEffect(() => {
+    axios
+      .get("https://www.adorebeauty.com.au/api/ecommerce/catalog/products")
+      .then((res) => {
+        setProducts(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, [axios]);
+
   return (
     <div className="root">
       <header className="app-header">
         <AdoreLogo className="logo" />
       </header>
       <main className="app-content">
-        { isLoading && <CircularProgress className="app-loader" /> }
-        { !isLoading && <ProductList /> }
+        {isLoading && <CircularProgress className="app-loader" />}
+        {!isLoading && products.length === 0 && <p>Sorry, no products found.</p>}
+        {!isLoading && products.length > 0 && <ProductList products={products} />}
       </main>
-      <footer className="app-footer">{DEVELOPER} - {YEAR}</footer>
+      <footer className="app-footer">
+        {DEVELOPER} - {YEAR}
+      </footer>
     </div>
   );
 }
--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -1,11 +1,119 @@
-function ProductList() {
-  return (
-    <ul className="product-list">
-      <li>product 1</li>
-      <li>product 2</li>
-      <li>product 3</li>
-    </ul>
-  );
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import { useEffect } from "react";
+
+const columns = [
+  { id: "id", label: "ID", minWidth: 20 },
+  { id: "renderName", label: "Name", minWidth: 180 },
+  {
+    id: "sku",
+    label: "SKU",
+    minWidth: 50,
+  },
+];
+
+function createData(id, name, sku) {
+  const hyphenIndex = name.indexOf("-");
+  const renderName = hyphenIndex
+    ? name.slice(hyphenIndex + 2, name.length).trim()
+    : name;
+  return { id, renderName, sku };
 }
 
-export default ProductList;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "90vw",
+    margin: "0 auto",
+    [theme.breakpoints.down("sm")]: {
+      width: "100vw",
+      fontSize: "0.8rem",
+    },
+  },
+  container: {
+    maxHeight: 650,
+  },
+}));
+
+export default function ProductList({ products }) {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rows, setRows] = React.useState([]);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  useEffect(() => {
+    for (let i = 0; i < products?.length; i++) {
+      setRows((prevState) => [
+        createData(products[i].id, products[i].name, products[i].sku),
+        ...prevState,
+      ]);
+    }
+  }, [products]);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
