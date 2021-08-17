40034f64ebb4d1d2e6f0c8d7a23e22f55ce711f6
innovativeGem <innovativeGem@gmail.com>
2021-08-17 20:49:08 +1000

    loader added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.scss';
 import {ReactComponent as AdoreLogo} from './adore-logo.svg';
 import ProductList from './components/product/ProductList';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { useState } from 'react';
 
 const DEVELOPER = 'Naman Nanavati';
 const TODAY = new Date();
 const YEAR = TODAY.getFullYear();
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+
+  // Temporary
+  setTimeout(() => {
+    setLoading(false);
+  }, 500);
   return (
     <div className="root">
       <header className="app-header">
         <AdoreLogo className="logo" />
       </header>
       <main className="app-content">
-        <ProductList />
+        { isLoading && <CircularProgress className="app-loader" /> }
+        { !isLoading && <ProductList /> }
       </main>
       <footer className="app-footer">{DEVELOPER} - {YEAR}</footer>
     </div>
