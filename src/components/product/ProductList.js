d7942860142f911b7f6400680c93e36fc135657b
innovativeGem <innovativeGem@gmail.com>
2021-08-17 23:56:11 +1000

    refactor

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -88,7 +88,7 @@ export default function ProductList({ products }) {
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
